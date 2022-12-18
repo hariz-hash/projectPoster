083f7e3279b61ec2d48574d76cd716d4bac75443
hariz <hariznyp@gmail.com>
2022-12-18 16:49:34 +0000

    able to update

--- routes/posters.js
+++ routes/posters.js
@@ -45,4 +45,58 @@ router.post('/create', async(req,res)=>{
     })
 })
 
+router.get('/:poster_id/update', async (req, res) => {
+    // retrieve the product
+    const posterId = req.params.poster_id
+    const poster = await Posters.where({
+        'id': posterId
+    }).fetch({
+        require: true,
+    });
+
+    const posterForm = createPostersForm();
+
+    // fill in the existing values
+    posterForm.fields.title.value = poster.get('title');
+    posterForm.fields.cost.value = poster.get('cost');
+    posterForm.fields.description.value = poster.get('description');
+    posterForm.fields.date.value = poster.get('date');
+    posterForm.fields.stock.value = poster.get('stock');
+    posterForm.fields.height.value = poster.get('height');
+    posterForm.fields.width.value = poster.get('width');
+
+    res.render('posters/update', {
+        'form': posterForm.toHTML(bootstrapField),
+        'poster': poster.toJSON()
+    })
+
+})
+
+router.post('/:poster_id/update', async (req, res) => {
+
+    // fetch the product that we want to update
+    const poster = await Posters.where({
+        'id': req.params.poster_id
+    }).fetch({
+        require: true
+    });
+
+    // process the form
+    const postersForm = createPostersForm();
+    postersForm.handle(req, {
+        'success': async (form) => {
+            poster.set(form.data);
+            poster.save();
+            res.redirect('/products');
+        },
+        'error': async (form) => {
+            res.render('posters/update', {
+                'form': form.toHTML(bootstrapField),
+                'poster': poster.toJSON()
+            })
+        }
+        
+    })
+
+})
 module.exports = router;
