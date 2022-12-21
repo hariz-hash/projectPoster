4e82f1ffb2e81700c1aefdeae8961209b8f055ec
hariz <hariznyp@gmail.com>
2022-12-21 15:47:37 +0000

    added sessions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const hbs = require("hbs");
 const wax = require("wax-on");
+const session = require('express-session');
+const flash = require('connect-flash');
+const FileStore = require('session-file-store')(session);
 require("dotenv").config();
 
 // create an instance of express app
@@ -19,9 +22,37 @@ wax.setLayoutPath("./views/layouts");
 // enable forms
 app.use(
   express.urlencoded({
-    extended: false
+    store: new FileStore(),
+    extended: false,
+    secret:'keyboard cat',
+    resave: false,
+    saveUninitialized: true
   })
 );
+
+app.use(session({
+  store: new FileStore(),
+  secret: process.env.SESSION_SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(flash())
+
+// Register Flash middleware
+app.use(function (req, res, next) {
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.error_messages = req.flash("error_messages");
+  next();
+});
+
+// Register Flash middleware
+app.use(function (req, res, next) {
+    res.locals.success_messages = req.flash("success_messages");
+    res.locals.error_messages = req.flash("error_messages");
+    next();
+});
+
 // import in routes
 const landingRoutes = require('./routes/landing');
 const postersRoutes = require('./routes/posters')
